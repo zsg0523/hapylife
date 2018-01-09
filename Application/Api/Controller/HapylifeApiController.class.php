30cda8b28ce41ec57df36e059f30a7de2c01ba0b
Qtian <863388577@qq.com>
2018-01-09 18:14:24 +0800

    zqt201819就注册

--- Application/Api/Controller/HapylifeApiController.class.php
+++ Application/Api/Controller/HapylifeApiController.class.php
@@ -6,18 +6,59 @@ use Common\Controller\HomeBaseController;
 **/
 class HapylifeApiController extends HomeBaseController{
 	
-	public function index(){}
+	public function index(){
+		$data = D('Users')->select();
+		// foreach ($data as $key => $value) {
+		// 	$tmpe             = $value;
+		// 	$tmpe['LastName'] = trim($value['LastName']);
+		// 	$tmpe['FirstName'] = trim($value['FirstName']);
+		// 	$tmpe['CustomerID'] = trim($value['CustomerID']);
+		// 	$add = D('User')->add($tmpe);
+		// }
+		// if($add){
+		// 	echo '添加完毕';
+		// }
+		p($data);
+	}
 	/**
-	* 注册
+	* 旧用户注册
 	**/
-	public function register(){}
+	public function oldregister(){
+		$data = I('post.');
+		$where= array(
+			'LastName'     =>$data['LastName'],
+			'FirstName'    =>$data['FirstName'],
+			'CustomerID'   =>$data['CustomerID'],
+			'WeeklyVolume' =>1
+		);
+		$find = D('Users')->where($where)->find();
+		if($find){
+			// $tmpe = array(
+			// 	'Phone'     =>$data['Phone'],
+			// 	'PassWord'  =>md5($data['PassWord']),
+			// 	'JustIdcard'=>$data['JustIdcard'],
+			// 	'BackIdcard'=>$data['BackIdcard']
+			// );
+			// $save = D('Users')->where($where)->save($tmpe);
+			// if($save){
+				$data['status'] = 1;
+				$this->ajaxreturn($data);				
+			// }else{
+			// 	$data['status'] = 0;
+			// 	$this->ajaxreturn($data);
+			// }
+		}else{
+			$data['status'] = 0;
+			$this->ajaxreturn($data);
+		}
+	}
 
 	/**
 	* 登录
 	**/
 	public function login(){}
-
 	/**
+	* 
 	* 置顶新闻
 	**/
 	public function newstop(){
