e57d5f8364653b0b20e162b8bab8e86b9dcd0c6b
Eden <shenggen93@163.com>
2018-01-09 11:51:37 +0800

    zsg

---
+++ Application/Api/Controller/HaptlifeApiController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Api\Controller;
+use Common\Controller\HomeBaseController;
+/**
+* hapylife控制器
+**/
+class HapylifeApiController extends HomeBaseController{
+	public function index(){
+		
+	}
+	
+}
