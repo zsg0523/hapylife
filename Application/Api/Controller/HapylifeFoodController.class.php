e3d2bb353a02437dbdc38846a294b15804766c3e
Eden <shenggen93@163.com>
2018-02-13 09:56:48 +0800

    zsg

---
+++ Application/Api/Controller/HapylifeFoodController.class.php
@@ -0,0 +1,80 @@
+<?php
+namespace Api\Controller;
+use Common\Controller\HomeBaseController;
+/**
+* hapylife food控制器
+**/
+class HapylifeFoodController extends HomeBaseController{
+	/**
+	* index操作
+	**/
+	public function index(){
+
+	}
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
