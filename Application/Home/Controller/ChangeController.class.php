59db31a9ba7ed593e9e65556a0fd30b1a9c550d4
Jun-Tin <262323675@qq.com>
2018-07-16 18:30:21 +0800

    fix

--- Application/Home/Controller/ChangeController.class.php
+++ Application/Home/Controller/ChangeController.class.php
@@ -9,20 +9,19 @@ class ChangeController extends HomeBaseController{
     * 修改密码
     **/ 
     public function changePsd(){
-        // 账号
-        $CustomerID = strtoupper(trim(I('post.CustomerID')));
+        $iuid = $_SESSION['user']['id'];
         // 原密码
         $psd = md5(trim(I('post.password')));
         // 用户信息
-        $userinfo = M('User')->where(array('CustomerID'=>$CustomerID))->find();
+        $userinfo = M('User')->where(array('iuid'=>$iuid))->find();
+        // 新密码
+        $new_psd['PassWord'] = md5(trim(I('post.passwords')));
         if($userinfo['password'] != $psd){
             // 密码不正确
             $sample['status'] = 2;
             $this->ajaxreturn($sample);
         }else{
-            // 新密码
-            $new_psd['PassWord'] = md5(trim(I('post.passowrds')));   
-            $result = M('User')->where(array('CustomerID'=>$CustomerID))->save($new_psd);
+            $result = M('User')->where(array('iuid'=>$iuid))->save($new_psd);
             if($result){
                 // 修改成功
                 $sample['status'] = 1;
