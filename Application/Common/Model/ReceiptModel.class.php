3827f5dc526d066b8054723c4eb333bedb3ba859
Jun-Tin <262323675@qq.com>
2018-07-23 18:31:44 +0800

    fix

--- Application/Common/Model/ReceiptModel.class.php
+++ Application/Common/Model/ReceiptModel.class.php
@@ -15,115 +15,33 @@ class ReceiptModel extends BaseModel{
      * @return array            分页数据
      */
     public function getPage($model,$word,$starttime,$endtime,$status,$order='',$timeType,$limit=20){
-        switch ($status) {
-            case '-1':
-                if(empty($word)){
-                    $count=$model->where(array($timeType=>array(array('egt',$starttime),array('elt',$endtime))))->count();
-                    p($count);
-                    die;
-                }else{
-                    $count=$model
-                    ->alias('r')
-                    ->join('hapylife_user u on r.riuid = u.iuid')
-                    ->where(array('r.rCustomerID|ir_receiptnum|inner_trade_no|trade_status|ir_price|u.LastName|u.FirstName|ips_trade_no|ips_trade_status'=>array('like','%'.$word.'%'),$timeType=>array(array('egt',$starttime),array('elt',$endtime))))
-                    ->count();
-                }
-                $page=new_page($count,$limit);
-                // 获取分页数据
-                if(empty($word)){
-                    if (empty($field)) {
-                        $list=$model
-                            ->alias('r')
-                            ->join('hapylife_user u on r.riuid = u.iuid')
-                            ->where(array($timeType=>array(array('egt',$starttime),array('elt',$endtime))))
-                            ->order($order)
-                            ->limit($page->firstRow.','.$page->listRows)
-                            ->select();         
-                    }else{
-                        $list=$model
-                            ->alias('r')
-                            ->join('hapylife_user u on r.riuid = u.iuid')
-                            ->where(array($timeType=>array(array('egt',$starttime),array('elt',$endtime))))
-                            ->field($field)
-                            ->order($order)
-                            ->limit($page->firstRow.','.$page->listRows)
-                            ->select();
-                    }
-                }else{
-                    if (empty($field)) {
-                        $list=$model
-                            ->alias('r')
-                            ->join('hapylife_user u on r.riuid = u.iuid')
-                            ->where(array('r.rCustomerID|ir_receiptnum|inner_trade_no|trade_status|ir_price|u.LastName|u.FirstName|ips_trade_no|ips_trade_status'=>array('like','%'.$word.'%'),$timeType=>array(array('egt',$starttime),array('elt',$endtime))))
-                            ->order($order)
-                            ->limit($page->firstRow.','.$page->listRows)
-                            ->select();         
-                    }else{
-                        $list=$model
-                            ->alias('r')
-                            ->join('hapylife_user u on r.riuid = u.iuid')
-                            ->field($field)
-                            ->where(array('r.rCustomerID|ir_receiptnum|inner_trade_no|trade_status|ir_price|u.LastName|u.FirstName|ips_trade_no|ips_trade_status'=>array('like','%'.$word.'%'),$timeType=>array(array('egt',$starttime),array('elt',$endtime))))
-                            ->order($order)
-                            ->limit($page->firstRow.','.$page->listRows)
-                            ->select();         
-                    }
-                }
-                break;
-            default:
-                if(empty($word)){
-                    $count=$model->where(array('ir_status'=>$status,$timeType=>array(array('egt',$starttime),array('elt',$endtime))))->count();
-                }else{
-                    $count=$model
-                    ->alias('r')
-                    ->join('hapylife_user u on r.riuid = u.iuid')
-                    ->where(array('ir_status'=>array('in',$status),'r.rCustomerID|ir_receiptnum|inner_trade_no|trade_status|ir_price|u.LastName|u.FirstName|ips_trade_no|ips_trade_status'=>array('like','%'.$word.'%'),$timeType=>array(array('egt',$starttime),array('elt',$endtime))))
-                    ->count();
-                }
-                $page=new_page($count,$limit);
-                // 获取分页数据
-                if(empty($word)){
-                    if (empty($field)) {
-                        $list=$model
-                            ->alias('r')
-                            ->join('hapylife_user u on r.riuid = u.iuid')
-                            ->where(array('ir_status'=>array('in',$status),$timeType=>array(array('egt',$starttime),array('elt',$endtime))))
-                            ->order($order)
-                            ->limit($page->firstRow.','.$page->listRows)
-                            ->select();         
-                    }else{
-                        $list=$model
-                            ->alias('r')
-                            ->join('hapylife_user u on r.riuid = u.iuid')
-                            ->where(array('ir_status'=>array('in',$status),$timeType=>array(array('egt',$starttime),array('elt',$endtime))))
-                            ->field($field)
-                            ->order($order)
-                            ->limit($page->firstRow.','.$page->listRows)
-                            ->select();         
-                    }
-                }else{
-                    if (empty($field)) {
-                        $list=$model
-                            ->alias('r')
-                            ->join('hapylife_user u on r.riuid = u.iuid')
-                            ->where(array('ir_status'=>array('in',$status),'r.rCustomerID|ir_receiptnum|inner_trade_no|trade_status|ir_price|u.LastName|u.FirstName|ips_trade_no|ips_trade_status'=>array('like','%'.$word.'%'),$timeType=>array(array('egt',$starttime),array('elt',$endtime))))
-                            ->order($order)
-                            ->limit($page->firstRow.','.$page->listRows)
-                            ->select();         
-                    }else{
-                        $list=$model
-                            ->alias('r')
-                            ->join('hapylife_user u on r.riuid = u.iuid')
-                            ->field($field)
-                            ->where(array('ir_status'=>array('in',$status),'r.rCustomerID|ir_receiptnum|inner_trade_no|trade_status|ir_price|u.LastName|u.FirstName|ips_trade_no|ips_trade_status'=>array('like','%'.$word.'%'),$timeType=>array(array('egt',$starttime),array('elt',$endtime))))
-                            ->order($order)
-                            ->limit($page->firstRow.','.$page->listRows)
-                            ->select();         
-                    }
-                }
-                break;
+        $count=$model
+            ->alias('r')
+            ->join('hapylife_user u on r.riuid = u.iuid')
+            ->where(array('ir_status'=>array('in',$status),'r.rCustomerID|ir_receiptnum|inner_trade_no|trade_status|ir_price|u.LastName|u.FirstName|ips_trade_no|ips_trade_status'=>array('like','%'.$word.'%'),$timeType=>array(array('egt',$starttime),array('elt',$endtime))))
+            ->count();
+        // p($count);die;
+        $page=new_page($count,$limit);
+        // 获取分页数据
+        if (empty($field)) {
+            $list=$model
+                ->alias('r')
+                ->join('hapylife_user u on r.riuid = u.iuid')
+                ->where(array('ir_status'=>array('in',$status),'r.rCustomerID|ir_receiptnum|inner_trade_no|trade_status|ir_price|u.LastName|u.FirstName|ips_trade_no|ips_trade_status'=>array('like','%'.$word.'%'),$timeType=>array(array('egt',$starttime),array('elt',$endtime))))
+                ->order($order)
+                ->limit($page->firstRow.','.$page->listRows)
+                ->select();
+        }else{
+            $list=$model
+                ->alias('r')
+                ->join('hapylife_user u on r.riuid = u.iuid')
+                ->field($field)
+                ->where(array('ir_status'=>array('in',$status),'r.rCustomerID|ir_receiptnum|inner_trade_no|trade_status|ir_price|u.LastName|u.FirstName|ips_trade_no|ips_trade_status'=>array('like','%'.$word.'%'),$timeType=>array(array('egt',$starttime),array('elt',$endtime))))
+                ->order($order)
+                ->limit($page->firstRow.','.$page->listRows)
+                ->select();         
         }
-            
+
         $data=array(
             'data'=>$list,
             'page'=>$page->show()
@@ -270,7 +188,7 @@ class ReceiptModel extends BaseModel{
     }
 
     public function export_excel($data){
-        $title   = array('创建日期','创建时间','用户ID','订单号','畅捷订单号','畅捷订单状态','订单状态','订单总价','订货人','收货人','收货地址','收货人电话','产品数量','产品信息');
+        $title   = array('创建日期','创建时间','用户ID','订单号','畅捷订单号','畅捷订单状态','IPS编号','IPS订单状态','订单状态','订单总价','订货人','收货人','收货地址','收货人电话','产品数量','产品信息');
         foreach ($data as $k => $v) {
             $content[$k]['ir_date']        = date('Y-m-d',$v['ir_date']);
             $content[$k]['ir_time']        = date('H:i:s',$v['ir_date']);
@@ -278,15 +196,29 @@ class ReceiptModel extends BaseModel{
             $content[$k]['ir_receiptnum']  = $v['ir_receiptnum'];
             $content[$k]['inner_trade_no'] = $v['inner_trade_no'];
             $content[$k]['trade_status']   = $v['trade_status'];
+            $content[$k]['ips_trade_no']   = $v['ips_trade_no'];
+            $content[$k]['ips_trade_status']   = $v['ips_trade_status'];
             switch ($v['ir_status']) {
                 case '0':
                     $content[$k]['ir_status'] = '待付款';
                     break;
                 case '2':
-                    $content[$k]['ir_status'] = '已支付';
+                    $content[$k]['ir_status'] = '已支付待发货';
                     break;
                 case '3':
-                    $content[$k]['ir_status'] = '交易完成';
+                    $content[$k]['ir_status'] = '已发货待收货';
+                    break;
+                case '4':
+                    $content[$k]['ir_status'] = '已送达';
+                    break;
+                case '5':
+                    $content[$k]['ir_status'] = '已申请退货';
+                    break;
+                case '7':
+                    $content[$k]['ir_status'] = '待付款';
+                    break;
+                case '8':
+                    $content[$k]['ir_status'] = '已退货';
                     break;
             }
             $content[$k]['ir_price']       = $v['ir_price'];
@@ -381,11 +313,11 @@ class ReceiptModel extends BaseModel{
             // 创建日期
             $content[$k]['ir_datetime']  = date('Y-m-d',$v['ir_date']);
             // 创建时间
-            $content[$k]['ir_datetime']  = date('H:i:s',$v['ir_date']);
+            $content[$k]['ir_datetimes']  = date('H:i:s',$v['ir_date']);
             // 支付日期
             $content[$k]['ir_paytime']   = date('Y-m-d',$v['ir_paytime']);
             // 支付时间
-            $content[$k]['ir_paytime']   = date('H:i:s',$v['ir_paytime']);
+            $content[$k]['ir_paytimes']   = date('H:i:s',$v['ir_paytime']);
 
             if(empty($v['send_time'])){
                 $content[$k]['send_time'] = '暂未发货';
