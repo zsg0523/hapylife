1f9f97613374ae0f62243d2840d7e8f53586a1e2
Eden <shenggen93@163.com>
2018-07-27 16:19:52 +0800

    zsg

--- Application/Common/Model/SmscodeModel.class.php
+++ Application/Common/Model/SmscodeModel.class.php
@@ -9,7 +9,7 @@ class SmscodeModel extends BaseModel{
     *腾讯云发送短信(纯发送短信)
     *参数：phoneNumber(手机号),acnumber(区号)
     **/
-    public function sms($appid='1400094198',$appkey='81ab38811e166f44ec8b77e09ee7c032',$phoneNumber,$acnumber,$templateId,$smsSign,$params){
+    public function sms($appid='1400096409',$appkey='fc1c7e21ab36fef1865b0a3110709c51',$phoneNumber,$acnumber,$templateId,$smsSign,$params){
         // require __DIR__ . "/vendor/autoload.php";
         vendor('SmsSing.SmsSingleSender');
         // 指定模板ID单发短信
